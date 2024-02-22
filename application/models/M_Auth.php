6883e6b136765b123cd750aff03d37082b54d2b7
praga arya <aryapragar@gmail.com>
2024-02-22 13:22:47 +0700

    disable register

--- application/models/M_Auth.php
+++ application/models/M_Auth.php
@@ -10,17 +10,16 @@ class M_Auth extends CI_MODEL
     return $this->db->get_where('user', ['username' => $uname])->row();
   }
 
-  public function register()
-  {
-    $data = array(
-      'nama'      => $this->input->post('nama'),
-      'username'  => $this->input->post('username'),
-      'pass'      => password_hash($this->input->post('pass'), PASSWORD_DEFAULT),
-      // 'pass'   => password_hash('hira-to-admpassku', PASSWORD_DEFAULT),
-      'role'      => 'admin',
-      'dateAdd'   => date('Y-m-d H:i:s')
-    );
+  // public function register()
+  // {
+  //   $data = array(
+  //     'nama'      => $this->input->post('nama'),
+  //     'username'  => $this->input->post('username'),
+  //     'pass'      => password_hash($this->input->post('pass'), PASSWORD_DEFAULT),
+  //     'role'      => 'petugas',
+  //     'dateAdd'   => date('Y-m-d H:i:s')
+  //   );
 
-    $this->db->insert('user', $data);
-  }
+  //   $this->db->insert('user', $data);
+  // }
 }
